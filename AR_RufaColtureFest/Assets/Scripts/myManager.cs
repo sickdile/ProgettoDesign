be242bc55f03bc136a0bc84cc0f4076c5a5e19b1
ER DIO SUPREMO SALAMINO EUROSPIN <151382285+CandyPusher@users.noreply.github.com>
2026-04-08 16:31:23 +0200

    frewg

--- AR_RufaColtureFest/Assets/Scripts/myManager.cs
+++ AR_RufaColtureFest/Assets/Scripts/myManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -7,32 +8,53 @@ public class myManager : MonoBehaviour
 
     [SerializeField]
     ARTrackedImageManager m_ImageManager;
+    [SerializeField]
+    ARAnchorManager m_AnchorManager;
     [SerializeField] GameObject[] Anchors;
 
-    [SerializeField] GameObject scenaDaVisualizzare;
+    [SerializeField] GameObject anchorPos;
 
     Transform[] checkpoints = new Transform[2];
 
-    void OnEnable() => m_ImageManager.trackablesChanged.AddListener(OnChanged);
+    [SerializeField] TextMeshProUGUI debug;
 
-    void OnDisable() => m_ImageManager.trackablesChanged.RemoveListener(OnChanged);
+    void OnEnable()
+    {
+        m_ImageManager.trackablesChanged.AddListener(OnChanged);
+        m_AnchorManager.trackablesChanged.AddListener(OnAnchorChanged);
+    }
+
+    void OnDisable()
+    {
+        m_ImageManager.trackablesChanged.RemoveListener(OnChanged);
+        m_AnchorManager.trackablesChanged.RemoveListener(OnAnchorChanged);
+    }
+
+    bool hasInstantiated;
+
+    void OnAnchorChanged(ARTrackablesChangedEventArgs<ARAnchor> eventArgs)
+    {
+        foreach (var anchor in eventArgs.updated)
+        {
+            string text = "ID: " + anchor.trackableId.ToString() + " Sub1: " + anchor.trackableId.subId1.ToString() + " Sub2: " + anchor.trackableId.subId2.ToString();
+            debug.SetText(text);
+        }
+    }
 
     void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
         foreach (var newImage in eventArgs.added)
         {
             Debug.Log("New image detected!");
-            if (newImage.referenceImage.name == "MKR_0")
+            if (newImage.referenceImage.name == "MKR_0" && !hasInstantiated)
             {
-                checkpoints[0] = Instantiate(Anchors[0], newImage.transform).transform;
-                scenaDaVisualizzare.SetActive(true);
-                scenaDaVisualizzare.transform.parent = checkpoints[0];
+                Instantiate(anchorPos, newImage.transform);
+                hasInstantiated = true;
             }
-            else if (newImage.referenceImage.name == "MKR_1")
+            else if (newImage.referenceImage.name == "MKR_1" && !hasInstantiated)
             {
-                checkpoints[1] = Instantiate(Anchors[1], newImage.transform).transform;
-                scenaDaVisualizzare.transform.parent = checkpoints[1];
-
+                Instantiate(anchorPos, newImage.transform);
+                hasInstantiated = true;
             }
         }
 
