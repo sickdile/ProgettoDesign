6cc49209c51b501c547431aa7755e1ff8bfda013
EHEFHNERh <diletta.nannini@unirufa.it>
2026-04-15 16:42:19 +0200

    setup progetto AR

---
+++ AR_SaloneMobile/Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System.Collections.Generic;
+
+public class InputManager : MonoBehaviour
+{
+    ARRaycastManager raycastManager;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    [SerializeField] SO_Events refTo_SO_Events;
+
+    private void Start()
+    {
+        raycastManager = GetComponentInParent<ARRaycastManager>();
+    }
+
+    public void OnTapInput(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            Debug.Log("Tap ricevuto!");
+            Vector2 screenPos = ctx.ReadValue<Vector2>();
+
+            Debug.Log("Cliccato su " + screenPos);
+            if (raycastManager.Raycast(screenPos, hits, TrackableType.PlaneWithinPolygon))
+            {
+                Pose hitPose = hits[0].pose;
+                Debug.Log("Inviato evento");
+
+                refTo_SO_Events.evt_placeObject.Invoke(hitPose);
+            }
+        }
+    }
+
+}
+
