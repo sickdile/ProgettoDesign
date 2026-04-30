44579c3195e33f43072d91573d01a69900055d42
OICIAODILéTTA <diletta.nannini@unirufa.it>
2026-04-30 09:30:35 +0200

    cosa al fly

---
+++ AR_SaloneMobile/Assets/Scripts/AutoAssignAdaptivePadding.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoAssignAdaptivePadding : MonoBehaviour
+{
+    [SerializeField] HorizontalLayoutGroup HLG;
+    [SerializeField] RectTransform sampleListItem;
+
+    private void Start()
+    {
+        HLG = GetComponent<HorizontalLayoutGroup>();
+        HLG.padding.left = Mathf.RoundToInt(.5f * (Screen.width - sampleListItem.rect.width));
+    }
+}
